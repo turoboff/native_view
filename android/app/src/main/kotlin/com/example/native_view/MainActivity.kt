9aebd4992540b6234faa09d68540b2836d4a9b48
Turoboff <turoboff.f@gmail.com>
2021-09-20 00:48:02 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/native_view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.native_view
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        flutterEngine
+                .platformViewsController
+                .registry
+                .registerViewFactory("<platform-view-type>", NativeViewFactory())
+    }
+}
---
+++ android/app/src/main/kotlin/com/example/native_view/NativeView.kt
@@ -0,0 +1,24 @@
+package com.example.native_view
+
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.widget.TextView
+import io.flutter.plugin.platform.PlatformView
+
+internal class NativeView(context: Context, id: Int, creationParams: Map<String?, Any?>?) : PlatformView {
+    private val textView: TextView
+
+    override fun getView(): View {
+        return textView
+    }
+
+    override fun dispose() {}
+
+    init {
+        textView = TextView(context)
+        textView.textSize = 72f
+        textView.setBackgroundColor(Color.rgb(255, 0, 255))
+        textView.text = "Rendered on a native Android view (id: $id)"
+    }
+}
